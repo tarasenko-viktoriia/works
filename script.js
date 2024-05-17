fe51d865cf99e5bde3e772a8c3b77dbba1df2c34
victoriataracenko <victoriataracenko0412@gmail.com>
2024-05-17 11:14:24 +0300

    1

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ const buttonHoverColor = () =>{
     });    
 }
 buttonHoverColor()
+
+
+// const cardsContainer = document.querySelector(".container");
+
+// cardsContainer.addEventListener("click", (e) => {
+//   const target = e.target.closest(".card");
+
+//   if (!target) return;
+
+//   cardsContainer.querySelectorAll(".card").forEach((card) => {
+//     card.classList.remove("active");
+//   });
+
+//   target.classList.add("active");
+// });
